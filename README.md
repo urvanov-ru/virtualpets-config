7265b7401af82c7d802b3d942fb57f8284fd4d48
Fedor Urvanov <fedor@urvanov.ru>
2024-06-09 14:15:50 +0300

    Virtualpets Config  первый коммит

---
+++ README.md
@@ -0,0 +1,7 @@
+Virtualpets Config
+==================
+
+Пример использования Spring Cloud Config для книги [Spring Book 2024](https://urvanov.ru/%d0%ba%d0%bd%d0%b8%d0%b3%d0%b8/spring-book-2024/).
+
+Используется проектом [virtualpets-server-springboot](https://github.com/urvanov-ru/virtualpets-server-springboot).
+
---
+++ src/main/java/ru/urvanov/virtualpets/config/VirtualpetsConfigApplication.java
@@ -0,0 +1,15 @@
+package ru.urvanov.virtualpets.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class VirtualpetsConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualpetsConfigApplication.class, args);
+	}
+
+}
